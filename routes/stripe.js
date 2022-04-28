9945a5944d2bab42a964dce97621da0294a12234
svonohlen <83403193+svonohlen@users.noreply.github.com>
2022-04-28 10:46:01 -0700

    implemented stripe payment checkout

--- routes/stripe.js
+++ routes/stripe.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
-const stripe = require("stripe")("process.env.STRIPE_KEY");
-module.exports = router;
+require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_KEY);
 
 router.post("/payment", (req, res) => {
   stripe.charges.create(
@@ -8,7 +8,7 @@ router.post("/payment", (req, res) => {
       source: req.body.tokenId, // when any payment is done, stripe is returning tokenid
       amount: req.body.amount,
       currency: "cad",
-    },
+    }, //after this stripe is gonna return either error or charge object if charge successful
     (stripeErr, stripeRes) => {
       if (stripeErr) {
         res.status(500).json(stripeErr);
@@ -18,3 +18,5 @@ router.post("/payment", (req, res) => {
     }
   );
 });
+
+module.exports = router;
